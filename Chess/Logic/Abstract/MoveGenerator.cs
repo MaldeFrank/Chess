5aeb3aa447c5bf338923ac54e6f8fb79f8424c26
Malde <maldefrankmidtgaard@gmail.com>
2025-11-07 12:10:54 +0100

    SRP refac

--- Chess/Logic/Abstract/MoveGenerator.cs
+++ Chess/Logic/Abstract/MoveGenerator.cs
@@ -1,8 +1,9 @@
 using Chess.Models;
 
+
 namespace Chess.Logic.Abstract
 {
-    public abstract class MoveGenerator : BasicMoves
+    public abstract class MoveGenerator
     {
         /// <summary>
         /// Generates all the possible moves for the selected cell
@@ -14,92 +15,44 @@ namespace Chess.Logic.Abstract
         {
             var piece = cell.Occupant;
             if (piece == null) return new List<string>();
-            List<string> occupiedCells = findOccupiedCells(cellIds);
-            List<(int, int)> occupiedCellsCoords = ToCoords(occupiedCells);
+            List<string> occupiedCells = ChessBoardUtility.FindOccupiedCells(cellIds);
+            List<(int, int)> occupiedCellsCoords = ChessBoardUtility.ToCoords(occupiedCells);
 
             var baseMoves = GeneratePieceSpecificMoves(cell, occupiedCellsCoords);
 
-            return ToCellId(baseMoves);
+            return ChessBoardUtility.ToCellId(baseMoves);
         }
-
-        /// <summary>
-        ///  Converts a list of tuples (row, column) to cellids like "a1", "h8"
-        /// </summary>
-        /// <param name="ids">List of (row, column) </param>
-        /// <returns></returns>
-        private List<string> ToCellId(List<(int, int)> ids)
+        private bool Collision(int row, int col, List<(int, int)> occupiedCells)
         {
-            Dictionary<int, string> mappings = new Dictionary<int, string>();
-            mappings.Add(1, "a");
-            mappings.Add(2, "b");
-            mappings.Add(3, "c");
-            mappings.Add(4, "d");
-            mappings.Add(5, "e");
-            mappings.Add(6, "f");
-            mappings.Add(7, "g");
-            mappings.Add(8, "h");
+            (int, int) potentialMove = (row, col);
 
-            List<string> cellIds = new List<string>();
-
-            ids.ForEach(id =>
+            if (occupiedCells.Contains(potentialMove))
             {
-                string letter = mappings[id.Item1];
-                string number = id.Item2.ToString();
-                cellIds.Add(letter + number);
-            });
-
-            return cellIds;
-        }
-
-        /// <summary>
-        ///  Finds all occupied cells
-        /// </summary>
-        /// <param name="cellIds">A list of cells and their id.</param>
-        /// <returns> Returns all ids of occupied cells </returns>
-        private List<string> findOccupiedCells(Dictionary<string, Cell> cellIds)
-        {
-            List<string> occupiedCells = new List<string>();
-            foreach (KeyValuePair<string, Cell> pair in cellIds)
-            {
-                if (pair.Value.Occupant != null)
-                {
-                    occupiedCells.Add(pair.Key);
-                }
+                return true;
             }
-            return occupiedCells;
+            return false;
         }
 
-        /// <summary>
-        /// Converts a list of chess notations like "a1", "h8" into a set of (row, column) coordinates.
-        /// </summary>
-        /// <param name="cellIds">The list of chess notations (e.g., ["a1", "b2"]).</param>
-        /// <returns> A list of (row, column) coordinates. </returns>
-        private List<(int, int)> ToCoords(List<string> cellIds)
+        protected List<(int row, int col)> GetMovesInDirection(int startRow, int startCol, int maxSteps, List<(int, int)> occupiedCells, int rowDirec, int colDirec)
         {
-            Dictionary<string, int> mappings = new Dictionary<string, int>();
-            mappings.Add("a", 1);
-            mappings.Add("b", 2);
-            mappings.Add("c", 3);
-            mappings.Add("d", 4);
-            mappings.Add("e", 5);
-            mappings.Add("f", 6);
-            mappings.Add("g", 7);
-            mappings.Add("h", 8);
+            var moves = new List<(int, int)>();
+            int r = startRow + rowDirec;
+            int c = startCol + colDirec;
+            int steps = 0;
 
-            List<(int, int)> coords = new List<(int, int)>();
-
-            cellIds.ForEach(cellId =>
+            while (r >= 1 && r <= 8 && c >= 1 && c <= 8 && steps < maxSteps)
             {
-                string letterPart = cellId.Substring(0, 1).ToLower();
-                string numberPart = cellId.Substring(1);
+                if (Collision(r, c, occupiedCells))
+                {
+                    break;
+                }
+                moves.Add((r, c));
 
-                int row = mappings[letterPart];
-                int col = int.Parse(numberPart);
-
-                coords.Add((row, col));
-            });
-
-            return coords;
+                r += rowDirec;
+                c += colDirec;
+                steps++;
+            }
+            return moves;
         }
 
         /// <summary>
